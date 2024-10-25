65eadc3f3f7e3891f8598870e97b50cdb6831244
Alex Pace <156442400+alexxppp@users.noreply.github.com>
2024-10-26 00:32:11 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Parallax effect
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+        const backgroundText = document.querySelector('.background-text');
+        backgroundText.style.transform = `translate(-50%, calc(-50% + ${scrollPosition * 0.503}px))`;
+    });
+
+    const languages = {
+        Java: {
+            code: `
+    public <span class="keyword">static</span> void <span class="function">main</span>(String[] <span class="variable">args</span>) {
+        if (<span class="condition">you'd like to get in touch</span>) { 
+            <br>            <a href="mailto:pacealexwork@gmail.com">pacealexwork@gmail.com</a>; <br>
+        }
+        if (<span class="condition">you want to see my projects</span>) { 
+            <br>            <a href="https://github.com/alexxppp">Visit my Github</a>; <br>
+        }
+    }`
+        },
+        Kotlin: {
+            code: `
+    <span class="function">private</span> <span class="function">fun</span> <span class="function">main</span>() {
+        if (<span class="condition">you'd like to get in touch</span>) { 
+            <br>            <a href="mailto:pacealexwork@gmail.com">pacealexwork@gmail.com</a> <br>
+        }
+        if (<span class="condition">you want to see my projects</span>) { 
+            <br>            <a href="https://github.com/alexxppp">Visit my Github</a> <br>
+        }
+    }`
+        },
+        JavaScript: {
+            code: `
+    function <span class="function">main</span>() {
+        if (<span class="condition">you'd like to get in touch</span>) { 
+            <br>            <a href="mailto:pacealexwork@gmail.com">pacealexwork@gmail.com</a> <br>
+        }
+        if (<span class="condition">you want to see my projects</span>) { 
+            <br>            <a href="https://github.com/alexxppp">Visit my Github</a> <br>
+        }
+    }`
+        },
+        C: {
+            code: `
+    int <span class="function">main</span>(int <span class="variable">argc</span>, char* <span class="variable">argv</span>[]) {
+        if (<span class="condition">you'd like to get in touch</span>) { 
+            <br>            <a href="mailto:pacealexwork@gmail.com">pacealexwork@gmail.com</a>; <br>
+        }
+        if (<span class="condition">you want to see my projects</span>) { 
+            <br>            <a href="https://github.com/alexxppp">Visit my Github</a>; <br>
+        }
+    }`
+        },
+        Python: {
+            code: `
+    def <span class="function">main</span>():
+        if <span class="condition">you'd like to get in touch</span>: 
+            <br>            <a href="mailto:pacealexwork@gmail.com">pacealexwork@gmail.com</a> <br>
+        if <span class="condition">you want to see my projects</span>: 
+            <br>            <a href="https://github.com/alexxppp">Visit my Github</a>`
+        },
+        Dart: {
+            code: `
+    void <span class="function">main</span>() {
+        if (<span class="condition">you'd like to get in touch</span>) { 
+            <br>            <a href="mailto:pacealexwork@gmail.com">pacealexwork@gmail.com</a>; <br>
+        }
+        if (<span class="condition">you want to see my projects</span>) { 
+            <br>            <a href="https://github.com/alexxppp">Visit my Github</a>; <br>
+        }
+    }`
+        }
+    };
+    
+    const codeBlock = document.getElementById('code-block');
+    const languageButtons = document.querySelectorAll('.language-button');
+    
+    languageButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Remover clase activa de otros botones
+            languageButtons.forEach(btn => btn.classList.remove('active'));
+            
+            // Agregar clase activa al botón seleccionado
+            button.classList.add('active');
+    
+            // Cambiar el contenido del bloque de código y el color
+            const selectedLang = button.getAttribute('data-lang');
+            codeBlock.innerHTML = languages[selectedLang].code;
+            codeBlock.style.color = languages[selectedLang].color;
+        });
+    });
+    
+});
